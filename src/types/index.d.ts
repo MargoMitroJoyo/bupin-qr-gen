c4f5c1ddfd7dc7c93e9b69a910d1fe6412b8716a
Tegar Aditya <tegaraditya53@gmail.com>
2025-02-13 10:42:54 +0700

    Add TypeScript interfaces for QR code and video information structures

---
+++ src/types/index.d.ts
@@ -0,0 +1,30 @@
+export interface InfoUJN {
+  kodeQRUjian: string
+  idUjian: string
+  idJenjang: string
+  idKelas: string
+  idMapel: string
+  idBab: string
+  namaJenjang: string
+  namaKelas: string
+  namaBab: string
+  namaMapel: string
+}
+
+export interface InfoVID {
+  kode_qr: string
+  nama_jenjang: string
+  nama_kelas: string
+  nama_mapel: string
+  nama_bab: string
+  nama_sub_bab: string
+  id_kelas: string
+  id_mapel: string
+  id_bab: string
+  id_sub_bab: string
+  link_video: string
+  ytid: string
+  link_dmp: any
+  ytid_dmp: any
+  tp: string
+}
