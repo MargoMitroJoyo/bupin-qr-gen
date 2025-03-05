a6baa9ae411df3ee6d3a740bf95bf91987f632ba
Tegar Aditya <tegaraditya53@gmail.com>
2025-03-05 14:35:09 +0700

    feat: refactor QR code generation to use generateImage function

--- src/handlers/qr.ts
+++ src/handlers/qr.ts
@@ -1,8 +1,7 @@
 import { createFactory } from "hono/factory"
-import { QRCodeErrorCorrectionLevel } from "qrcode"
 import { zValidator } from "@hono/zod-validator"
 import { z } from "zod"
-import { addTextOverlay, compressImage, generateQRCode, getFileName } from "../utils"
+import { generateImage, getFileName } from "../utils"
 
 const factory = createFactory()
 
@@ -28,22 +27,35 @@ export const getQRImage = factory.createHandlers(
     const fileNameFromInfo = await getFileName(id as string)
     if (!fileNameFromInfo) return c.text("QR code not found.", 404)
 
-    try {
-      const canvas = await generateQRCode(url, detail as QRCodeErrorCorrectionLevel)
-      addTextOverlay(canvas)
-      const compressedBuffer = await compressImage(canvas, format)
-      const contentType = format === "jpeg" ? "image/jpeg" : "image/png"
-      const fileName = `${fileNameFromInfo}.${format === "jpeg" ? "jpg" : "png"}`
+    return generateImage(c, url, format, detail, fileNameFromInfo)
+  }
+)
 
-      return c.body(compressedBuffer, {
-        headers: {
-          "Content-Type": contentType,
-          "Content-Disposition": `inline; filename="${fileName}"`,
-        },
-      })
-    } catch (error) {
-      console.error("Error generating QR code:", error)
-      return c.text("Failed to generate QR code.", 500)
-    }
+const universalQrValidationSchema = {
+  params: z.object({
+    string: z.string(),
+  }),
+  query: z.object({
+    format: z.enum(["png", "jpeg", "avif"]).optional().default("png"),
+    detail: z.enum(["low", "medium", "high"]).optional().default("high"),
+    filename: z.string().optional(),
+    watermark: z.enum(["true", "false"]).optional().default("true"),
+  }),
+}
+
+export const getUniversalQRImage = factory.createHandlers(
+  zValidator("param", universalQrValidationSchema.params),
+  zValidator("query", universalQrValidationSchema.query),
+  async (c) => {
+    const string = c.req.param("string") as string
+    const format = c.req.query("format") || "png"
+    const detail = c.req.query("detail") || "high"
+    const name = c.req.query("filename") || String(Bun.hash(string))
+    const watermark = c.req.query("watermark")
+    const url = String(string)
+
+    const isWatermarked = watermark ? (watermark === "false" ? false : true) : true
+
+    return generateImage(c, url, format, detail, name, isWatermarked)
   }
 )
