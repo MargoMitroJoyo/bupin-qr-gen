8114d366e171fc70cc7c476b1e2bac863316166d
Tegar Aditya <tegaraditya53@gmail.com>
2025-10-20 16:09:06 +0700

    feat: add BSE QR image route and ISBN search functionality

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono"
-import { getQRImage, getUniversalQRImage } from "../handlers/qr"
+import { getBseQRImage, getQRImage, getUniversalQRImage } from "../handlers/qr"
+import { searchByISBN } from "../lib/bse"
 
 const api = new Hono()
 
 api.get("/qr/:id", ...getQRImage)
 api.get("/qr/u/:string", ...getUniversalQRImage)
+api.get("/qr/bse/:isbn", ...getBseQRImage)
+
+api.get("/r/bse/:isbn", (c) => {
+  const isbn = c.req.param("isbn")
+  const book = searchByISBN(isbn)
+  return c.redirect(`https://buku.kemendikdasmen.go.id/katalog/${book?.slug}`, 302)
+})
 
 export default api
