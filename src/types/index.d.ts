eaceffc34aebe59707a3c5aad81c7c9573d9a39b
Tegar Aditya <tegaraditya53@gmail.com>
2025-10-20 16:09:09 +0700

    feat: extend Book interface with additional properties for comprehensive book details

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -15,3 +15,49 @@ export interface InfoVID {
   nama_bab: string
   nama_sub_bab: string
 }
+
+export interface Book {
+  id: number;
+  title: string;
+  slug: string;
+  image: string | null;
+  attachment: string | null;
+  description: string | null;
+  published_year: string | null;
+  class: string | null;
+  level: string | null;
+  writer: string | null;
+  reviewer: string | null;
+  translator: string | null;
+  adapter: string | null;
+  designer: string | null;
+  cover_designer: string | null;
+  ilustrator: string | null;
+  editor: string | null;
+  aligner: string | null;
+  publisher: string | null;
+  contributor: string | null;
+  language: string | null;
+  context: string | null;
+  subject: string | null;
+  format: string | null;
+  isbn: string | null;
+  curriculum: string | null;
+  collation: string | null;
+  type: string | null;
+  edition: string | null;
+  unit: string | null;
+  status: string | null;
+  category: string | null;
+  book_type: string | null;
+  version: string | null;
+  price_zone_1: number | null;
+  price_zone_2: number | null;
+  price_zone_3: number | null;
+  price_zone_4: number | null;
+  price_zone_5A: number | null;
+  price_zone_5B: number | null;
+  created_at: string | null;
+  updated_at: string | null;
+  deleted_at: string | null;
+}
