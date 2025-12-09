d20c6c9c0ec301939d7e01e722235d65263a30d0
Tegar Aditya <tegaraditya53@gmail.com>
2025-12-09 15:11:36 +0700

    feat: integrate PrismaMariaDb adapter for database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,27 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaMariaDb } from "@prisma/adapter-mariadb"
+import { PrismaClient } from "../generated/prisma/client"
 
-export const prisma = new PrismaClient({
-  errorFormat: "pretty",
+const dbDetails = parseDatabaseUrl(process.env.DATABASE_URL!)
+
+const adapter = new PrismaMariaDb({
+  host: dbDetails.host,
+  port: dbDetails.port,
+  user: dbDetails.user,
+  password: dbDetails.password,
+  database: dbDetails.database,
 })
+
+export const prisma = new PrismaClient({ adapter })
+
+// UTILITIES TO PARSE DATABASE URLS
+function parseDatabaseUrl(connectionString: string) {
+  const url = new URL(connectionString)
+
+  return {
+    host: url.hostname,
+    port: Number(url.port),
+    user: url.username,
+    password: url.password,
+    database: url.pathname.slice(1),
+  }
+}
